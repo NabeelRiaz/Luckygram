e6ec019e67b087ac18cf65c6dec4f835428f7d7f
Nabeels-MacBook-Pro <nabeelriaz_147@hotmaiL.com>
2015-05-26 14:19:53 -0400

    clean up

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,2 +1,10 @@
 class Pin < ActiveRecord::Base
+	belongs_to :user
+	# Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :pins
 end
